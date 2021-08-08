a4fee0ef76908d5153b906474b7b006f6fe306a0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 11:36:40 +0200

    Zad3 draft

---
+++ src/main/java/com/sda/advanced/solution/zad3/Main.java
@@ -0,0 +1,30 @@
+package com.sda.advanced.solution.zad3;
+
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//old school - mutable map
+        /*Map<String, Integer> map = new HashMap<>();
+        map.put("Java", 18);
+        map.put("C#", 9);
+        map.put("Python", 1);
+        map.put("JavaScript", 0);
+        map.put("PHP", 0);*/
+
+		//java 11 style - immutable map
+		Map<String, Integer> map = Map.of("Java", 18,
+				"C#", 9,
+				"Python", 1,
+				"JavaScript", 0,
+				"PHP", 0);
+
+		print(map);
+	}
+
+	private static void print(Map<String, Integer> map) {
+
+	}
+
+}
