76b6fbb06e17387975c21fc77f699b5f57bac85a
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 12:31:46 +0200

    Zad4 draft

---
+++ src/main/java/com/sda/advanced/solution/zad4/Storage.java
@@ -0,0 +1,18 @@
+package com.sda.advanced.solution.zad4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+
+	private final Map<String, List<String>> storage;
+
+	public Storage() {
+		storage = new HashMap<>();
+	}
+
+	public void addToStorage(String key, String value) {
+
+	}
+}
