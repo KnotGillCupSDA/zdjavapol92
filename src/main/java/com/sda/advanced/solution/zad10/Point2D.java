e939b3c8944dc098b69dbc13fa228904870ebc88
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 15:46:54 +0200

    Zad10 point is movable

--- src/main/java/com/sda/advanced/solution/zad10/Point2D.java
+++ src/main/java/com/sda/advanced/solution/zad10/Point2D.java
@@ -1,6 +1,6 @@
 package com.sda.advanced.solution.zad10;
 
-public class Point2D {
+public class Point2D implements Movable {
 
     private double x;
     private double y;
@@ -25,4 +25,10 @@ public class Point2D {
     public void setY(double y) {
         this.y = y;
     }
+
+	@Override
+	public void move(MoveDirection moveDirection) {
+		x = x + moveDirection.getX();
+		y = y + moveDirection.getY();
+	}
 }
