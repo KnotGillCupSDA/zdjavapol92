1b8a3aadddbb85e2866f50e16f3ac20abf2a9b5b
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 10:47:34 +0200

    Zad1 done, different approaches

--- src/main/java/com/sda/advanced/solution/zad1/Main.java
+++ src/main/java/com/sda/advanced/solution/zad1/Main.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> unsorted = List.of("A", "B", "b", "Z", "a", "c");
-		List<String> sorted = sortNamed(unsorted);
-		System.out.println(sorted);
+		List<String> sorted = sortOnList(unsorted);
+
+		System.out.println("unsorted: "+ unsorted);
+		System.out.println("sorted: "+ sorted);
 
 	}
 
@@ -33,6 +36,34 @@ public class Main {
 
 		//2 - named Comparator interface implementation
 		Collections.sort(sorted, new MyReversedComparator());
+		return sorted;
+	}
+
+	private static List<String> sortLambda(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+
+		//3 - with lambda, since java 8
+		Collections.sort(sorted, (o1, o2) -> -o1.compareTo(o2));
+		return sorted;
+	}
+
+	private static List<String> sortStream(List<String> unsorted) {
+		//4 - with streams, since java 8
+		return unsorted.stream()
+				//.sorted(new MyReversedComparator())
+				.sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
+	}
+
+	private static List<String> sortOnList(List<String> unsorted) {
+		List<String> sorted = new ArrayList<>(unsorted);
+
+		//5 - directly on List, since java 11
+		//sorted.sort(new MyReversedComparator());
+		//sorted.sort((o1, o2) -> -o1.compareTo(o2));
+
+		//with built-in reversed order comparator
+		sorted.sort(Comparator.reverseOrder());
 		return sorted;
 	}
 
--- src/main/java/com/sda/advanced/solution/zad1/MyReversedComparator.java
+++ src/main/java/com/sda/advanced/solution/zad1/MyReversedComparator.java
@@ -6,6 +6,7 @@ public class MyReversedComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String o1, String o2) {
+		System.out.println("o1: "+ o1 + " o2: "+ o2);
 		return -o1.compareTo(o2);
 	}
 }
