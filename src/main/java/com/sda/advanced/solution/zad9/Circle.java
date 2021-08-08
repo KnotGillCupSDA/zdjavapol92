c0a4c938e9e53d609aafa25c5fdb0ef14cec355e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 15:38:34 +0200

    Zad9 done

--- src/main/java/com/sda/advanced/solution/zad9/Circle.java
+++ src/main/java/com/sda/advanced/solution/zad9/Circle.java
@@ -2,4 +2,25 @@ package com.sda.advanced.solution.zad9;
 
 public class Circle {
 
+	private final Point2D center;
+	private final Point2D point;
+
+	public Circle(Point2D center, Point2D point) {
+		this.center = center;
+		this.point = point;
+	}
+
+	public double getRadius() {
+		return Math.sqrt(
+				Math.pow(center.getX() - point.getX(), 2)
+						+ Math.pow(center.getY() - point.getY(), 2));
+	}
+
+	public double getPerimeter() {
+		return 2 * Math.PI * getRadius();
+	}
+
+	public double getArea() {
+		return Math.PI * Math.pow(getRadius(), 2);
+	}
 }
---
+++ src/main/java/com/sda/advanced/solution/zad9/Main.java
@@ -0,0 +1,12 @@
+package com.sda.advanced.solution.zad9;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Circle circle = new Circle(new Point2D(0.0, 0.0), new Point2D(1.0, 0.0));
+
+		System.out.println("radius: " + circle.getRadius());
+		System.out.println("perimeter: " + circle.getPerimeter());
+		System.out.println("area: " + circle.getArea());
+	}
+}
