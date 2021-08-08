11c6e31d0daaee7dacb9f72206d56f443cb46bfa
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 11:44:57 +0200

    Zad3 printing raw entries

--- src/main/java/com/sda/advanced/solution/zad3/Main.java
+++ src/main/java/com/sda/advanced/solution/zad3/Main.java
@@ -24,7 +24,8 @@ public class Main {
 	}
 
 	private static void print(Map<String, Integer> map) {
-
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
 	}
-
 }
