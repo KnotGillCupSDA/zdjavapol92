8a8de79fad9d81302ca5d1d47096646ba1474c3c
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 14:44:09 +0200

    Zad8 done

--- src/main/java/com/sda/advanced/solution/zad8/ValidatorImpl.java
+++ src/main/java/com/sda/advanced/solution/zad8/ValidatorImpl.java
@@ -4,6 +4,26 @@ public class ValidatorImpl implements Validator {
 
 	@Override
 	public boolean validate(Parcel parcel) {
-		return false;
+
+		if (parcel.getxLength() + parcel.getyLength() + parcel.getyLength() > 300) {
+			return false;
+		}
+
+		if (parcel.getxLength() <= 30
+				|| parcel.getyLength() <= 30
+				|| parcel.getzLength() <= 30) {
+			return false;
+		}
+
+		if(parcel.isExpress() && parcel.getWeight() > 15.0) {
+			return false;
+		}
+
+		if(!parcel.isExpress() && parcel.getWeight() > 30.0) {
+			return false;
+		}
+
+		return true;
 	}
+
 }
--- src/test/java/com/sda/advanced/solution/zad8/ValidatorImplTest.java
+++ src/test/java/com/sda/advanced/solution/zad8/ValidatorImplTest.java
@@ -1,6 +1,10 @@
 package com.sda.advanced.solution.zad8;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,13 +19,44 @@ class ValidatorImplTest {
 	}
 
 	@Test
-	void shouldReturnFalseWhenSumOfLengthsAbove300() {
-		//when
-		final Parcel parcel = new Parcel(101, 101, 101, 1.0f, false);
+	void testStandardParcelHappyPath() {
+		Parcel parcel = new Parcel(31, 32, 33, 19.0f, false);
+		assertTrue(validator.validate(parcel));
+	}
 
-		final boolean isValid = validator.validate(parcel);
+	@Test
+	void testExpressParcelHappyPath() {
+		Parcel parcel = new Parcel(31, 32, 33, 13.0f, true);
+		assertTrue(validator.validate(parcel));
+	}
 
-		//then
-		assertFalse(isValid);
+	@Test
+	void thatIsNotValidWhenSumOfLengthsIsTooHigh() {
+		Parcel parcel = new Parcel(240, 31, 32, 1, false);
+		assertFalse(validator.validate(parcel));
+	}
+
+	@Test
+	void thatIsNotValidWhenAnyDimensionIsTooLow() {
+		List<Parcel> parcels = Arrays.asList(
+				new Parcel(1, 31, 31, 1, false),
+				new Parcel(31, 1, 31, 1, false),
+				new Parcel(31, 31, 1, 1, false));
+
+		for (Parcel parcel : parcels) {
+			assertFalse(validator.validate(parcel));
+		}
+	}
+
+	@Test
+	void thatIsNotValidWhenTooHeavyStandard() {
+		Parcel parcel = new Parcel(10, 10, 10, 34, false);
+		assertFalse(validator.validate(parcel));
+	}
+
+	@Test
+	void thatIsNotValidWhenTooHeavyExpress() {
+		Parcel parcel = new Parcel(10, 10, 10, 17, true);
+		assertFalse(validator.validate(parcel));
 	}
 }
