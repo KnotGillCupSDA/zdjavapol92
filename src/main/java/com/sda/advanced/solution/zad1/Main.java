e13ca7b53e97931cbfa34c913ec8dafd12bcab3b
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-08 10:10:58 +0200

    Zad1 draft

---
+++ src/main/java/com/sda/advanced/solution/zad1/Main.java
@@ -0,0 +1,21 @@
+package com.sda.advanced.solution.zad1;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<String> unsorted = List.of("A", "B", "b", "Z", "a", "c");
+
+		List<String> sorted = sort(unsorted);
+
+		System.out.println(sorted);
+
+	}
+
+	private static List<String> sort(List<String> unsorted) {
+		return null;
+	}
+
+}
