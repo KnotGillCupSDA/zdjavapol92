1cb753dc20ec3e8963c9909e75ee6471edfe0992
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-22 13:13:01 +0200

    zad30 done

--- src/main/java/com/sda/advanced/solution/zad30/Main.java
+++ src/main/java/com/sda/advanced/solution/zad30/Main.java
@@ -1,11 +1,15 @@
 package com.sda.advanced.solution.zad30;
 
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		final Path zad30Dir = Path.of("src", "main", "resources", "zad30");
 
 		final Path inputFile = zad30Dir.resolve("someTextFile.txt");
@@ -17,11 +21,11 @@ public class Main {
 
 		//if file exists
 		//read file content
-		String content = getFileContent(inputFile);
+		List<String> content = getFileContent(inputFile);
 		System.out.println(content);
 
 		//reverse file content
-		String reversedContent = reverseContent(content);
+		List<String> reversedContent = reverseContent(content);
 		System.out.println(reversedContent);
 
 		//reverse file name
@@ -32,20 +36,23 @@ public class Main {
 		saveFile(outputFile, reversedContent);
 	}
 
-	private static String getFileContent(Path inputFile) {
-		return null;
+	private static List<String> getFileContent(Path inputFile) throws IOException {
+		return Files.readAllLines(inputFile, StandardCharsets.UTF_8);
 	}
 
-	private static String reverseContent(String content) {
-		return null;
+	private static List<String> reverseContent(List<String> content) {
+		return content.stream()
+				.map( s -> new StringBuilder(s).reverse().toString())
+				.collect(Collectors.toList());
 	}
 
 	private static Path reverseFilename(Path inputFile) {
-		return null;
+		final String reversedName = new StringBuilder(inputFile.toFile().getName()).reverse().toString();
+		return inputFile.getParent().resolve(reversedName);
 	}
 
-	private static void saveFile(Path outputFile, String reversedContent) {
-
+	private static void saveFile(Path outputFile, List<String> content) throws IOException {
+		Files.write(outputFile, content, StandardCharsets.UTF_8);
 	}
 
 }
