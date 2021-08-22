717a08cced50f382c28358a8383b976655f0a245
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-22 10:50:08 +0200

    Zad27 done

---
+++ src/main/java/com/sda/advanced/solution/zad27/Joiner2.java
@@ -0,0 +1,15 @@
+package com.sda.advanced.solution.zad27;
+
+public class Joiner2 {
+
+	public <T> String join(String delimiter, T... parts) {
+		StringBuilder builder = new StringBuilder();
+		for (T part : parts) {
+			builder.append(part.toString());
+			builder.append(delimiter);
+		}
+		builder.deleteCharAt(builder.length()-1);
+		return builder.toString();
+	}
+
+}
