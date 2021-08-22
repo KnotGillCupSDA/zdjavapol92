4373a1a6dc939858e1ed2d995cb171455bb74299
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-22 12:16:48 +0200

    Zad30 draft3

--- src/main/java/com/sda/advanced/solution/zad30/Main.java
+++ src/main/java/com/sda/advanced/solution/zad30/Main.java
@@ -7,10 +7,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		final Path zad30Dir = Path.of("src", "main", "resources", "zad30");
+
 		final Path inputFile = zad30Dir.resolve("someTextFile.txt");
 
-		final Path outputFile = zad30Dir.resolve("output.txt");
+		//if file exists
+		//read file content
+		String content = getFileContent(inputFile);
+		//reverse file content
+		String reversedContent = reverseContent(content);
+		//reverse file name
+		Path outputFile = reverseFilename(inputFile);
+		//save reversed content under reversed name
+		saveFile(outputFile, reversedContent);
+		// else
+		// write a message
+	}
 
+	private static String getFileContent(Path inputFile) {
+		return null;
+	}
+
+	private static String reverseContent(String content) {
+		return null;
+	}
+
+	private static Path reverseFilename(Path inputFile) {
+		return null;
+	}
+
+	private static void saveFile(Path outputFile, String reversedContent) {
 
 	}
 
