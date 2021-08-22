a20d1db063eac0efe97300f03c2ffd4689de0074
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-08-22 11:58:40 +0200

    Zad30 draft2

--- src/main/java/com/sda/advanced/solution/zad30/Main.java
+++ src/main/java/com/sda/advanced/solution/zad30/Main.java
@@ -1,8 +1,16 @@
 package com.sda.advanced.solution.zad30;
 
+import java.io.FileInputStream;
+import java.nio.file.Path;
+
 public class Main {
 
 	public static void main(String[] args) {
+		final Path zad30Dir = Path.of("src", "main", "resources", "zad30");
+		final Path inputFile = zad30Dir.resolve("someTextFile.txt");
+
+		final Path outputFile = zad30Dir.resolve("output.txt");
+
 
 	}
 
