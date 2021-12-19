254c938b3ddbd3d752964c323658cb87d3747f67
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 13:18:05 +0100

    RomanNumeralConverter well tested

--- src/test/java/com/sda/testing/solution/parametrized/romannumeral/RomanNumeralConverterTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/romannumeral/RomanNumeralConverterTest.java
@@ -1,5 +1,7 @@
 package com.sda.testing.solution.parametrized.romannumeral;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -9,12 +11,13 @@ class RomanNumeralConverterTest {
 	@ParameterizedTest
 	@EnumSource
 	void shouldConvertSimpleValuesToRomanNotation(RomanNumeralConverter.ArabicToRoman arabicToRoman) {
-
+		assertEquals(arabicToRoman.getRoman(),
+				RomanNumeralConverter.romanFor(arabicToRoman.getArabic()));
 	}
 
 	@ParameterizedTest
 	@CsvSource({"234, CCXXXIV", "4999, MMMMCMXCIX", "767, DCCLXVII"})
 	void shouldConvertComplexValuesToRomanNotation(int arabic, String expectedRoman) {
-
+		assertEquals(expectedRoman, RomanNumeralConverter.romanFor(arabic));
 	}
 }
