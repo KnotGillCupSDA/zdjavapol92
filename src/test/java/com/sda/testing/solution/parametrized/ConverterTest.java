1283a579c5bf391b2f352a69854948e050ba9532
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 14:02:24 +0100

    MethodSource playground

--- src/test/java/com/sda/testing/solution/parametrized/ConverterTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/ConverterTest.java
@@ -8,7 +8,6 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -39,7 +38,7 @@ public class ConverterTest {
 
 	@ParameterizedTest
 	@EnumSource(value = ConversionType.class,
-			names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
+			names = { "METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS" })
 	void someOperationsShouldConvertToHigherValue(ConversionType conversionType) {
 		int i = new Random().nextInt(1000);
 		assertTrue(converter.convert(i, conversionType) > i);
@@ -48,46 +47,73 @@ public class ConverterTest {
 	@ParameterizedTest
 	@MethodSource("conversionTypePairs")
 	void someOperationsShouldBeReversible(ConversionTypeWithFunction conversionType1,
+			ConversionTypeWithFunction conversionType2) {
+
+		double initialValue = 10.0;
+		double convert = converter.convert(initialValue, conversionType1);
+		double finalValue = converter.convert(convert, conversionType2);
+
+		assertEquals(initialValue, finalValue);
+	}
+
+	private static Stream<Arguments> conversionTypePairs() {
+		return Stream.of(
+				Arguments.of(
+						ConversionTypeWithFunction.YARDS_TO_METERS,
+						ConversionTypeWithFunction.METERS_TO_YARDS
+				),
+				Arguments.of(
+						ConversionTypeWithFunction.METERS_TO_YARDS,
+						ConversionTypeWithFunction.YARDS_TO_METERS
+				),
+				Arguments.of(
+						ConversionTypeWithFunction.INCHES_TO_CENTIMETERS,
+						ConversionTypeWithFunction.CENTIMETERS_TO_INCHES
+				),
+				Arguments.of(
+						ConversionTypeWithFunction.CENTIMETERS_TO_INCHES,
+						ConversionTypeWithFunction.INCHES_TO_CENTIMETERS
+				),
+				Arguments.of(
+						ConversionTypeWithFunction.KILOMETERS_TO_MILES,
+						ConversionTypeWithFunction.MILES_TO_KILOMETERS
+				),
+				Arguments.of(
+						ConversionTypeWithFunction.MILES_TO_KILOMETERS,
+						ConversionTypeWithFunction.KILOMETERS_TO_MILES
+				)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("conversionTypePairsFancy")
+	void someOperationsShouldBeReversiblePlayground(
+			ConversionTypeWithFunction conversionType1,
 			ConversionTypeWithFunction conversionType2,
-			double initialValue,
-			Converter converter,
-			String errorMessage) {
+			double initialValue) {
 
 		double convert = converter.convert(initialValue, conversionType1);
 		double finalValue = converter.convert(convert, conversionType2);
 
-		assertEquals(initialValue, finalValue, errorMessage);
+		assertEquals(initialValue, finalValue);
 	}
 
-	public static Stream<Arguments> conversionTypePairs() {
+	public static Stream<Arguments> conversionTypePairsFancy() {
 		return Stream.of(
 				Arguments.of(
 						ConversionTypeWithFunction.YARDS_TO_METERS,
 						ConversionTypeWithFunction.METERS_TO_YARDS,
-						10.0,
-						new Converter(),
-						"Should be reversible"
+						10.0
 				),
 				Arguments.of(
-						ConversionTypeWithFunction.INCHES_TO_CENTIMETERS,
-						ConversionTypeWithFunction.CENTIMETERS_TO_INCHES,
-						10.0,
-						new Converter(),
-						"Should be reversible"
+						ConversionTypeWithFunction.YARDS_TO_METERS,
+						ConversionTypeWithFunction.METERS_TO_YARDS,
+						-11.2
 				),
 				Arguments.of(
-						ConversionTypeWithFunction.INCHES_TO_CENTIMETERS,
-						ConversionTypeWithFunction.CENTIMETERS_TO_INCHES,
-						100214321321.0,
-						new Converter(),
-						"Should be reversible"
-				),
-				Arguments.of(
-						ConversionTypeWithFunction.MILES_TO_KILOMETERS,
-						ConversionTypeWithFunction.KILOMETERS_TO_MILES,
-						10.0,
-						new Converter(),
-						"Should be reversible"
+						ConversionTypeWithFunction.YARDS_TO_METERS,
+						ConversionTypeWithFunction.METERS_TO_YARDS,
+						new Random().nextInt()
 				)
 		);
 	}
