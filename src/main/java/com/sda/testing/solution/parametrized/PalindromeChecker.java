52c472d08bfb0d57c81a19f909e18ba165cf34f1
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 11:54:31 +0100

    PalindromeChecker with CsvFileSource

--- src/main/java/com/sda/testing/solution/parametrized/PalindromeChecker.java
+++ src/main/java/com/sda/testing/solution/parametrized/PalindromeChecker.java
@@ -3,6 +3,7 @@ package com.sda.testing.solution.parametrized;
 public class PalindromeChecker {
 
 	public static boolean isPalindrome(String word) {
-		return new StringBuilder(word).reverse().toString().equals(word);
+		String sanitized = word.replaceAll(" ", "");
+		return new StringBuilder(sanitized).reverse().toString().equals(sanitized);
 	}
 }
--- src/test/java/com/sda/testing/solution/parametrized/PalindromeCheckerTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/PalindromeCheckerTest.java
@@ -3,6 +3,7 @@ package com.sda.testing.solution.parametrized;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class PalindromeCheckerTest {
@@ -11,6 +12,16 @@ class PalindromeCheckerTest {
 	//@ValueSource(strings = {"sedes","kajak"} )
 	@CsvSource({"sedes, true","kajak, true", "costam, false"})
 	void shouldVerifyWords(String word, boolean expected) {
+		//when
+		boolean actual = PalindromeChecker.isPalindrome(word);
+
+		//then
+		assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/testdata/palindrome.csv")
+	void shouldVerifyWordsFromExternalFile(String word, boolean expected) {
 		//when
 		boolean actual = PalindromeChecker.isPalindrome(word);
 
