681f0d416d0aca4e836bb3574dac8906bd6829b8
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 11:13:31 +0100

    PalindromeChecker implemented

--- src/main/java/com/sda/testing/solution/parametrized/PalindromeChecker.java
+++ src/main/java/com/sda/testing/solution/parametrized/PalindromeChecker.java
@@ -2,7 +2,7 @@ package com.sda.testing.solution.parametrized;
 
 public class PalindromeChecker {
 
-	public static boolean isPalindrome(String palindrome) {
-		return false;
+	public static boolean isPalindrome(String word) {
+		return new StringBuilder(word).reverse().toString().equals(word);
 	}
 }
