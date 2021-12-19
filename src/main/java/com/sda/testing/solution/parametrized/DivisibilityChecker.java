7a6ed54dcd663a8649b8e8dfcd6d4a82db873827
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 10:09:10 +0100

    DivisibilityChecker with first test

--- src/main/java/com/sda/testing/solution/parametrized/DivisibilityChecker.java
+++ src/main/java/com/sda/testing/solution/parametrized/DivisibilityChecker.java
@@ -2,4 +2,7 @@ package com.sda.testing.solution.parametrized;
 
 public class DivisibilityChecker {
 
+	public static boolean isDivisibleBy3(int number) {
+		return number % 3 == 0;
+	}
 }
--- src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
@@ -2,6 +2,19 @@ package com.sda.testing.solution.parametrized;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class DivisibilityCheckerTest {
 
+	@Test
+	void shouldBeDivisibleBy3() {
+		//given
+		int i = 9;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
+
+		//then
+		assertTrue(actual);
+	}
 }
