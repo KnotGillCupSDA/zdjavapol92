d8d61f5281aac8d11ee579da83c40242dba59d60
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 12:38:43 +0100

    @EnumSource example with beforeclass

--- src/test/java/com/sda/testing/solution/parametrized/ConverterTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/ConverterTest.java
@@ -1,27 +1,31 @@
 package com.sda.testing.solution.parametrized;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Random;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com.sda.advanced.solution.zad17.ConversionType;
 import com.sda.advanced.solution.zad17.Converter;
 
 public class ConverterTest {
 
+	private static Converter converter;
+
+	@BeforeAll
+	static void beforeAll() {
+		converter = new Converter();
+	}
+
 	@ParameterizedTest
 	@EnumSource
 	void shouldAlwaysConvertToDoubleWithinBoundaries(ConversionType conversionType) {
-		double convert = new Converter().convert(10003243.0, conversionType);
+		double convert = converter.convert(1032342, conversionType);
+
 		assertTrue(convert < Double.MAX_VALUE);
 		assertTrue(convert > Double.MIN_VALUE);
 		assertNotEquals(Double.NaN, convert);
@@ -32,6 +36,6 @@ public class ConverterTest {
 			names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
 	void someOperationsShouldConvertToHigherValue(ConversionType conversionType) {
 		int i = new Random().nextInt(1000);
-		assertTrue(new Converter().convert(i, conversionType) > i);
+		assertTrue(converter.convert(i, conversionType) > i);
 	}
 }
