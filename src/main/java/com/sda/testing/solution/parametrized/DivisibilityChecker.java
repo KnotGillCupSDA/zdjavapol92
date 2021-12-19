83527fe1c29ecc6dcc18694e36c23b3ef121e8b9
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 09:45:53 +0100

    DivisibilityChecker draft

---
+++ src/main/java/com/sda/testing/solution/parametrized/DivisibilityChecker.java
@@ -0,0 +1,5 @@
+package com.sda.testing.solution.parametrized;
+
+public class DivisibilityChecker {
+
+}
---
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
@@ -0,0 +1,7 @@
+package com.sda.testing.solution.parametrized;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisibilityCheckerTest {
+
+}
