c86cad56abf95e3df80d2a81cea2e6d46f3a2d53
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2021-12-19 09:45:26 +0100

    typo

--- src/main/java/com/sda/advanced/solution/zad17/ConversionTypeWithFunction.java
+++ src/main/java/com/sda/advanced/solution/zad17/ConversionTypeWithFunction.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public enum ConversionTypeWithFunction {
 	METERS_TO_YARDS(v -> v * 1.09361),
 	YARDS_TO_METERS(v -> v * 0.9144),
-	CENTIMETERS_TO_ICHES(v -> v * 0.393701),
+	CENTIMETERS_TO_INCHES(v -> v * 0.393701),
 	INCHES_TO_CENTIMETERS(v -> v * 2.54),
 	KILOMETERS_TO_MILES(v -> v * 0.621371),
 	MILES_TO_KILOMETERS(v -> v * 1.60934);
